0b46fd928d6387bda17cd65f6a6b547aa9f5ea8f
neofenril <arthurdu88@hotmail.com>
2015-12-06 23:22:58 +0100

    bonobo
    sync

--- modal.php
+++ modal.php
@@ -12,7 +12,7 @@
           <p><input type="text" class="span3" name="eid" id="email" placeholder="Email"></p>
           <p><input type="password" class="span3" name="passwd" placeholder="Password"></p>
           <p><button type="submit" class="btn btn-primary">Connexion</button>
-            <a href="https://www.google.fr/search?q=bonobo&espv=2&biw=1920&bih=955&site=webhp&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjxw_6PosjJAhXLnBoKHSp5BbAQ_AUIBigB">Forgot Password?</a>
+            <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Bonobo_sexual_behavior_1.jpg/1280px-Bonobo_sexual_behavior_1.jpg">Forgot Password?</a>
           </p>
         </form>
       </div>
