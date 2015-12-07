d8ee9c39ceeb8b874fa3c659cb559ebc712e4f65
neofenril <arthurdu88@hotmail.com>
2015-12-07 11:22:45 +0100

    PetitesModifs
    Changement du fichier navbar pour qu'il soit le seul fichier à gérer
    l'affichage de la barre de nav (avant y en avais aussi ans userpage), et
    création du repository (yohann en gros c'est la que ce feront tous les
    appels à la BDD)

--- navbar.php
+++ navbar.php
@@ -3,32 +3,41 @@
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <!-- Collect the nav links, forms, and other content for toggling -->
-          <a class="navbar-brand" href="#">Appartout'Atis</a>
-      <ul class="nav navbar-nav" id="access">
-        <li>
-          <a href="#">Meublés</a>
-        </li>
-        <li>
-          <a href="#">A l'année</a>
-        </li>
-        <li>
-          <a href="#">Vacances</a>
-        </li>
-        <li>
-          <a href="#">Locataire</a>
-        </li>
-        <li>
-          <a href="#">Propriétaire</a>
-        </li>
-      </ul>
-      <ul class="nav navbar-nav" id="identification">
-        <li>
-          <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-con">Connexion</button>
-        </li>
-        <li>
-          <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-ins">Inscription</button>
-        </li>
-      </ul>
+    <a class="navbar-brand" href="index.php">Appartout'Atis</a>
+    <ul class="nav navbar-nav" id="access">
+      <li>
+        <a href="#">Meublés</a>
+      </li>
+      <li>
+        <a href="#">A l'année</a>
+      </li>
+      <li>
+        <a href="#">Vacances</a>
+      </li>
+      <li>
+        <a href="#">Locataire</a>
+      </li>
+      <li>
+        <a href="UserPage.php">Propriétaire</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Services</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav" id="identification">
+      <li>
+        <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-con">Connexion</button>
+      </li>
+      <li>
+        <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-ins">Inscription</button>
+      </li>
+    </ul>
     <!-- /.navbar-collapse -->
   </div>
   <!-- /.container -->
