500b9b589276e2ca177de286d74a1125d3d5b110
Jakkos <julien.remy88@orange.fr>
2015-12-09 15:20:00 +0100

    affichappart

--- affich_appart.php
+++ affich_appart.php
@@ -1,68 +1,62 @@
-<div class="row text-center">
-      <div class="col-md-3 col-sm-6 hero-feature">
-        <div class="thumbnail">
-          <img src="http://placehold.it/800x500" alt="">
-          <div class="caption">
-            <h3>Feature Label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <p>
-              <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-            </p>
-          </div>
+<link href="css/heroic-features.css" rel="stylesheet">
+<?php 
+/*
+//$req = "update appartement set URLPHOTO='http://www.bauvey-immobilier.com/site/images/normal/l-ours-blanc-interieur-final-bd%20JALIS.jpg' WHERE IDAPPARTEMENT=2";
+$req = "select IDAPPARTEMENT,URLPHOTO from appartement";
+//$req = sp
+//$req = "select IDAPPARTEMENT, NUMAPPARTEMENT, URLPHOTO from appartement";
+  $result = odbc_exec($conn,$req);
+  
+  while(odbc_fetch_row($result)){
+  $name = odbc_result($result, 1);
+  $surname = odbc_result($result, 2);
+  print_r("$name $surname\n");
+}
+
+odbc_close($conn);/*
+*/
+include ('connexion.php');
+
+$req = find_appart(0);
+
+$result = odbc_exec($conn,$req);
+while(odbc_fetch_row($result)){
+  $titre = odbc_result($result, 1);
+  $surname = odbc_result($result, 2);
+  $photo = odbc_result($result, 3);  
+  imprimer($titre, $photo);
+}
+odbc_close($conn);
+
+function find_appart($type) {
+  if ($type==0) {
+    $req = 'SELECT APPARTTITRE, NUMAPPARTEMENT, URLPHOTO 
+    FROM appartement WHERE ESTDISPONIBLE=1';
+    return $req;
+  }
+  else {
+    $req = 'SELECT APPARTTITRE, NUMAPPARTEMENT, URLPHOTO FROM
+    appartement INNER JOIN type ON appartement.IDTYPE = type.IDTYPE 
+    WHERE ESTDISPONIBLE=1 AND appartement.IDTYPE='.$type;
+    return $req;
+  }
+}
+
+function imprimer($titre, $photo)  {
+  echo '
+    <div class="col-md-3 col-sm-6 hero-feature">
+      <div class="thumbnail">
+        <img src="'.$photo.'" alt="">
+        <div class="caption">
+          <h3>'.$titre.'</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p>
+            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+          </p>
         </div>
       </div>
-
-      <div class="col-md-3 col-sm-6 hero-feature">
-        <div class="thumbnail">
-          <img src="http://placehold.it/800x500" alt="">
-          <div class="caption">
-            <h3>Feature Label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-			</p>
-            <p>
-              <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-3 col-sm-6 hero-feature">
-        <div class="thumbnail">
-          <img src="http://placehold.it/800x500" alt="">
-          <div class="caption">
-            <h3>Feature Label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <p>
-              <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-3 col-sm-6 hero-feature">
-        <div class="thumbnail">
-          <img src="http://placehold.it/800x500" alt="">
-          <div class="caption">
-            <h3>Feature Label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <p>
-              <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-3 col-sm-6 hero-feature">
-        <div class="thumbnail">
-          <img src="http://placehold.it/800x500" alt="">
-          <div class="caption">
-            <h3>Feature Label</h3>
-            <p> <?php include("test.php") ?> </p>
-            <p>
-              <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-            </p>
-          </div>
-        </div>
-      </div>
-
     </div>
+  ';
+}
+?>
+
