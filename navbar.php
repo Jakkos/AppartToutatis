877e0a03a4db21bab274f7da0cbc72ebed93ca6f
neofenril <arthurdu88@hotmail.com>
2015-12-09 17:12:28 +0100

    BarreNavFonctionelleAvecFonction
    Ajout d'un switch case pour une gestion plus facile de la barre de
    navigation, affichage automatique des onglets disponnible en fonction du
    type de compte de la personne lors de la connexion

--- navbar.php
+++ navbar.php
@@ -1,46 +1,4 @@
 <?php include ("modal.php") ;?>
-<?php
-function genererBarreNav(){
-  if (isset($_SESSION["utilisateur"]))
-  {
-    if (isset($_SESSION["role"]))
-    {
-      $user=$_SESSION["role"];
-      switch ($user) {
-        case '0':
-          # ici ChefAgence
-        break;
-        case '1':
-          # ici EmployeAgence
-        break;
-        case '2':
-          # ici Locataire
-        break; 
-        case '3':
-          # ici Proprietaire
-        break;  
-        case '4':
-          # ici LocataireProprietaire
-        break;
-        case '5':
-          # ici UtilisateurConnecte
-        break;
-        
-        default:
-          # code...
-        break;
-      }  
-    }
-    else
-    {
-
-    }
-
-
-  }
-}
-
-?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -56,34 +14,83 @@ function genererBarreNav(){
       <li>
         <a href="#">Vacances</a>
       </li>
-      <li>
-        <a href="#">Locataire</a>
-      </li>
-      <li>
-        <a href="UserPage.php">Propriétaire</a>
-      </li>
-      <li>
-        <a href="#">About</a>
-      </li>
-      <li>
-        <a href="#">Services</a>
-      </li>
-      <li>
-        <a href="#">Contact</a>
-      </li>
-    </ul>
-    <ul class="nav navbar-nav" id="identification">
-      <li>
-        <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-con" >Connexion</button>
-      </li>
-      <li>
-        <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-ins">Inscription</button>
-      </li>
-    </ul>
-    <!-- /.navbar-collapse -->
-  </div>
-  <!-- /.container -->
-  <!-- /.container -->
-</nav>
+      <?php
+      $_SESSION["utilisateur"] = "oui";
+      $_SESSION["role"] = 8;
+      genererBarreNav();
+      ?> 
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+    <!-- /.container -->
+  </nav>
 
 
+  <?php
+  function genererBarreNav(){
+    if (isset($_SESSION["utilisateur"]))
+    {
+      if (isset($_SESSION["role"]))
+      {
+        $type=$_SESSION["role"];
+        switch ($type) {
+          case '0':
+          # ici ChefAgence
+          break;
+          case '1':
+          # ici EmployeAgence
+          break;
+          case '2':
+          echo <<<END
+          <li>
+          <a href="#">Locataire</a>
+          </li>
+END;
+          # ici Locataire
+          break; 
+          case '3':
+          echo <<<END
+          <li>
+          <a href="UserPage.php">Propriétaire</a>
+          </li>
+END;
+          # ici Proprietaire
+          break;  
+          case '4':
+          echo <<<END
+          <li>
+          <a href="#">Locataire</a>
+          </li>
+          <li>
+          <a href="UserPage.php">Propriétaire</a>
+          </li>
+END;
+          # ici LocataireProprietaire
+          break;
+          case '5':
+          # ici UtilisateurConnecte
+          break;
+
+          default:
+          echo <<<END
+          </ul>
+          <ul class="nav navbar-nav" id="identification">
+          <li>
+          <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-con" >Connexion</button>
+          </li>
+          <li>
+          <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-ins">Inscription</button>
+          </li>
+          </ul>
+END;
+          # code...
+          break;
+        }  
+      }
+      else
+      {
+
+      }
+    }
+  }
+  ?>
