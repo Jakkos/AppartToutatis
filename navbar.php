2c5bfbc8aefc2efae9ce34f5c03d720381b031d2
neofenril <arthurdu88@hotmail.com>
2015-12-09 15:42:43 +0100

    FonctionDansNavbar
    Ajout d'un switch case

--- navbar.php
+++ navbar.php
@@ -1,77 +1,89 @@
 <?php include ("modal.php") ;?>
- <?php
-    if (isset($_SESSION["utilisateur"]))
+<?php
+function genererBarreNav(){
+  if (isset($_SESSION["utilisateur"]))
+  {
+    if (isset($_SESSION["role"]))
     {
-  //test si les infos utilisateur sont toujours bonnes (si le mec a supprimer son compte et que cookie actif)$ par ex)
-
-      $user = $_SESSION["pseudo"];
+      $user=$_SESSION["role"];
       switch ($user) {
         case '0':
-          # ici le chef d'agence
+          # ici ChefAgence
         break;
         case '1':
-          # ici c'est pour les membres de l'agence
-        case '2':
-          # ici un locataire
-        break;  
-        case '3':
-          # ici un propriétaire
+          # ici EmployeAgence
         break;
+        case '2':
+          # ici Locataire
+        break; 
+        case '3':
+          # ici Proprietaire
+        break;  
         case '4':
-          # ici un ProprioLocataire
+          # ici LocataireProprietaire
+        break;
+        case '5':
+          # ici UtilisateurConnecte
         break;
         
         default:
           # code...
         break;
-      }
+      }  
+    }
+    else
+    {
 
+    }
+
+
+  }
 }
 
-      ?>
+?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <!-- Collect the nav links, forms, and other content for toggling -->
-      <a class="navbar-brand" href="index.php">Appartout'Atis</a>
-      <ul class="nav navbar-nav" id="access">
-        <li>
-          <a href="#">Meublés</a>
-        </li>
-        <li>
-          <a href="#">A l'année</a>
-        </li>
-        <li>
-          <a href="#">Vacances</a>
-        </li>
-        <li>
-          <a href="#">Locataire</a>
-        </li>
-        <li>
-          <a href="UserPage.php">Propriétaire</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-      <ul class="nav navbar-nav" id="identification">
-        <li>
-          <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-con" >Connexion</button>
-        </li>
-        <li>
-          <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-ins">Inscription</button>
-        </li>
-      </ul>
-      <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-    <!-- /.container -->
-  </nav>
+    <a class="navbar-brand" href="index.php">Appartout'Atis</a>
+    <ul class="nav navbar-nav" id="access">
+      <li>
+        <a href="#">Meublés</a>
+      </li>
+      <li>
+        <a href="#">A l'année</a>
+      </li>
+      <li>
+        <a href="#">Vacances</a>
+      </li>
+      <li>
+        <a href="#">Locataire</a>
+      </li>
+      <li>
+        <a href="UserPage.php">Propriétaire</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Services</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav" id="identification">
+      <li>
+        <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-con" >Connexion</button>
+      </li>
+      <li>
+        <button class= "btn btn-default navbar-btn" data-toggle="modal" data-target="#mod-ins">Inscription</button>
+      </li>
+    </ul>
+    <!-- /.navbar-collapse -->
+  </div>
+  <!-- /.container -->
+  <!-- /.container -->
+</nav>
 
 
--- test.php
+++ test.php
@@ -1,5 +1,5 @@
  <?php
- try {
+
 
 
  	echo 'This text is placed through <b>PHP</b>!';
@@ -7,71 +7,6 @@
  	echo '</p>';
 
  	include("connexion.php");
-
-<<<HEAD
- 	if ($conn) {
- 		//echo 'Connexion OK';
- 		//$req = "sp_help";
-
-
-//$req = "DELETE FROM adresse";
-$req = "SELECT NUMRUE,NOMRUE from adresse";
-//$req = "alter table adresse modify NOMRUE varchar(100)"; 
-//$req = "SELECT NOMDEP FROM departement";
-//$req = "delete from utilisateur";
-//$req=  "ALTER TABLE utilisateur modify TEL int NULL";
-// $req = "drop table utilisateur";	
-
-$req = "INSERT INTO utilisateur (IDUTILISATEUR, IDADRESSE, NOM, PRENOM, MAIL, TEL, PASSWORD) VALUES
-(1, 1, 'Alain', 'Guillaume', 'utynnattenn-1521@yopmail.', 6, 'Azarius');
-INSERT INTO utilisateur (IDUTILISATEUR, IDADRESSE, NOM, PRENOM, MAIL, TEL, PASSWORD) VALUES
-(2, 3, 'Alexis', 'Lucien', 'Anteponat@gmail.com', 00, 'Théogone');
-INSERT INTO utilisateur (IDUTILISATEUR, IDADRESSE, NOM, PRENOM, MAIL, TEL, PASSWORD) VALUES
-(3, 2, 'André', 'Damien', 'esdf@yahoo.fr', 00, 'Claudian');
-INSERT INTO utilisateur (IDUTILISATEUR, IDADRESSE, NOM, PRENOM, MAIL, TEL, PASSWORD) VALUES
-(4, 4, 'Antoine', 'Joseph', 'filll@yahoo.com', 00, 'Stéphanus');";
-$req = "SELECT IDUTILISATEUR from utilisateur";
-$req = "delete from utilisateur";
-$result = odbc_exec($conn,$req);
-
-
-
- /*while(odbc_fetch_row($result)){
-  $name = odbc_result($result, 1);
-  $surname = odbc_result($result, 2);
-  print_r("$name $surname\n");
-}*/
-
-
-
-  while ($data\[] = odbc_fetch_array($result));
-  print_r($data);
-
- 
-=======
-if ($conn) {
- 	echo 'Connexion OK';
- 	$req="select NOMRUE from adresse ";
- 	$result = odbc_exec($conn, $req);
- 	while ($data\[]=odbc_fetch_array($result));
- 	print_r($data);
->>>>>>> origin/site
- 	//$query = file_get_contents("appartements.sql");
-	//odbc_exec($conn, $query)or die(odbc_error());
-}
-else {
-	echo 'TMLC';
-}
-//!\
-odbc_close($conn);
-//!\
-}
-catch(Exception $e)
-{
-    // En cas d'erreur, on affiche un message et on arrête tout
-	die('Erreur : '.$e->getMessage());
-
-}
-HEAD
+ 	if($conn)
+ 		echo "ok";
 ?>
-<!-- /.container -->
