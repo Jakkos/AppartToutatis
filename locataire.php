206900d3f16db356d93f2e93467c4a2a6c9a0336
neofenril <arthurdu88@hotmail.com>
2015-12-16 22:04:31 +0100

    Tien Locataire
    rzegrtegtre

--- locataire.php
+++ locataire.php
@@ -1,11 +1,10 @@
+<body>
+  <div class="container">
+  	<div class="row text-center">
 <?php
 include 'administration/c_admin.php';
 include 'connexion.php';
-
-if ($conn)  {
-	echo 'ok';
-}
-
+include ('requete_appart.php');
 echo <<<END
 			<!DOCTYPE html>
 			<head>
@@ -26,23 +25,42 @@ echo <<<END
 END;
 include 'navbar.php';
 
-$req = "select * from sysusers;";
 
 
-
+$req = find_appart_loc(2);
 $result = odbc_exec($conn,$req);
 while(odbc_fetch_row($result)){
   $id = odbc_result($result, 1);
   $photo = odbc_result($result, 2);
-  $a = odbc_result($result, 3);
-  $b = odbc_result($result, 4);
-echo '<p>'.$id.'</p>';
-echo '<p>'.$photo.'</p>';
-echo '<p>'.$a.'</p>';
-echo '<p>'.$b.'</p>';
-echo '<p></p>';
+  $titre = get_description_titre($conn, $id);
+  $description = get_description_total($conn, $id);
+  imprimer($id, $titre, $photo, $description);
+}
+odbc_close($conn);
+
+function imprimer($id, $titre, $photo, $description)  {
+  echo '
+  <div class="col-md-3 col-sm-6 hero-feature">
+    <div class="thumbnail">
+      <img src="'.$photo.'" alt="">
+      <div class="caption">
+        <h3>'.utf8_decode($titre).'</h3>
+        <p>
+        <a href="appartement.php?id='.$id.'" class="btn btn-primary">Louer</a> <a href="#" class="btn btn-default">More Info</a>           
+        </p>
+      </div>
+    </div>
+  </div>
+  ';
 }
 // affichage de sa liste d'appartement
 // bouton payer
 // déposer contrat assurance et entretien chaudière
 ?>
+</div>
+<div class="row text-right">
+	 <p><button type="button" class="btn btn-default" data-dismiss="modal">Payer</button></p>
+	 <p><button type="button" class="btn btn-default" data-dismiss="modal">Entretien chaudière </button></p>
+</div>
+</div>
+</body>
