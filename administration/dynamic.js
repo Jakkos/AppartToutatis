9ad416c496611e35d1530164edd5941cacb165a4
Yohann Vaubourg <yohann.vaubourg@gmail.com>
2015-12-17 14:30:29 +0100

    emp

--- administration/dynamic.js
+++ administration/dynamic.js
@@ -18,4 +18,17 @@ function toggle_div(id, id2, id3) { // On déclare la fonction toggle_div qui pr
 	div3.style.display = "none"; // ... on masque l'autre
   }
 }
+
+function toggle_div(id, id2, id3, id4) { // On déclare la fonction toggle_div qui prend en param le bouton et un id
+  var div = document.getElementById(id); // On récupère le div ciblé grâce à l'id
+  var div2 = document.getElementById(id2);
+  var div3 = document.getElementById(id3);
+  var div4 = document.getElementById(id4); // on récupère le div 2 ciblé grâce à l'id
+  if(div.style.display=="none") { // Si le div est masqué...
+    div.style.display = "block"; // ... on l'affiche...
+	div2.style.display = "none"; // ... on masque l'autre
+	div3.style.display = "none"; // ... on masque l'autre
+	div4.style.display = "none"; // ... on masque l'autre
+  }
+}
 </script>
--- emp.php
+++ emp.php
@@ -49,13 +49,46 @@ if (isset($_POST['idSupC'])){
 	$res = odbc_exec($conn, $req);
 }
 if (isset($_POST['idValP'])){
-	$idValC = (is_numeric($_POST['idValP']) ? (int)$_POST['idValP'] : 0);
-	$req = "UPDATE dbo.contrat_a_prolonger SET DATEFINLOC = dateadd(yy, 3, DATEFINLOC) WHERE IDCONTRAT = $idValP";
+	$idValP = (is_numeric($_POST['idValP']) ? (int)$_POST['idValP'] : 0);
+	$req = "UPDATE dbo.contrat_a_prolonger SET DATEFINLOC = dateadd(yy, 3, DATEFINLOC), DMDPROLONG = 0 WHERE IDCONTRAT = $idValP";
 	$res = odbc_exec($conn, $req);
 }
 if (isset($_POST['idSupP'])){
-	$idSupC = (is_numeric($_POST['idSupP']) ? (int)$_POST['idSupP'] : 0);
+	$idSupP = (is_numeric($_POST['idSupP']) ? (int)$_POST['idSupP'] : 0);
 	$req = "UPDATE dbo.contrat_a_prolonger SET DMDPROLONG = 0 WHERE IDCONTRAT = $idSupP";
+	$res = odbc_exec($conn, $req);
+}
+if (isset($_POST['idValO'])){
+	$idValO = (is_numeric($_POST['idValO']) ? (int)$_POST['idValO'] : 0);
+	$mois = date('m');
+	$annee = date('Y');
+	if ($mois == 12){
+		$mois = '01';
+		$annee ++;
+	}else{
+		$mois++;
+	}
+	$deb = '01/'.$mois.'/'.$annee;
+	$req = "SELECT IDTYPE FROM dbo.appartement app, dbo.contratlocation con WHERE con.IDAPPARTEMENT = app.IDAPPARTEMENT AND con.IDCONTRAT = $idValO";
+	$res = odbc_exec($conn, $req);
+	$type = odbc_result($res, 1);
+
+	$fin = '31/06/';
+	if($type == 1){
+		$annee = $annee + 3;
+		$fin = '01/'.$mois.'/'.$annee;
+	}else if ($type == 3){
+		$annee = $annee + 1;
+		$fin = $fin = '31/06/'.$annee;
+	}
+	$req = "UPDATE dbo.contrat_a_valider SET DATEDEBUTLOC = convert(datetime, '$deb', 101) WHERE IDCONTRAT = $idValO";
+	$res = odbc_exec($conn, $req);
+	$req = "UPDATE dbo.contrat_a_valider SET DATEFINLOC = convert(datetime, '$fin', 101) WHERE IDCONTRAT = $idValO";
+	$res = odbc_exec($conn, $req);
+}
+if (isset($_POST['idSupO'])){
+	$idSupO = (is_numeric($_POST['idSupO']) ? (int)$_POST['idSupO'] : 0);
+	$req = "DELETE FROM dbo.contratlocation WHERE IDCONTRAT = $idSupO";
 	$res = odbc_exec($conn, $req);
 }
 
@@ -81,13 +114,16 @@ if (isset($_POST['idSupP'])){
 			  <nav class="navbar navbar-default" role="navigation" id="topmenu">
 				<ul class="nav navbar-nav">
 				  <li class="dropdown">
-					<a href="#" data-toggle="collapse" data-target="#one" onclick="toggle_div('app', 'con', 'bail');">Appartement</a>
+					<a href="#" data-toggle="collapse" data-target="#one" onclick="toggle_div('app', 'con', 'bail', 'opt');">Appartement</a>
 				  </li>
 				  <li class="dropdown">
-					<a href="#" data-toggle="collapse" data-target="#two" onclick="toggle_div('con', 'app', 'bail');">Contrat</a>
+					<a href="#" data-toggle="collapse" data-target="#two" onclick="toggle_div('con', 'app', 'bail', 'opt');">Contrat</a>
 				  </li>
 				   <li class="dropdown">
-					<a href="#" data-toggle="collapse" data-target="#two" onclick="toggle_div('bail', 'app', 'con');">Bail</a>
+					<a href="#" data-toggle="collapse" data-target="#two" onclick="toggle_div('bail', 'app', 'con', 'opt');">Bail</a>
+				  </li>
+				  <li class="dropdown">
+					<a href="#" data-toggle="collapse" data-target="#two" onclick="toggle_div('opt', 'bail', 'app', 'con');">Option</a>
 				  </li>
 				</ul>
 			  </nav>
@@ -189,6 +225,41 @@ END;
 			<td align="center">
 				<form action="emp.php" method="post">
 					<input type="hidden" name="idSupP" value="'.$contrat1.'">
+					<input type="image" src="img/delete.png" height="15" width="15">
+				</form>
+			</td>
+			</tr>';
+	}
+	echo <<<END
+						</table>
+					</div>
+				</div> 
+			</div>
+			<div id="opt" class="container" style="display:none;">
+				<div class="row">
+					<div class="col-md-12" style="border: 1px solid black;">
+						<h3 align="center">Poser Option</h4>
+END;
+	$req = "SELECT IDCONTRAT, IDAPPARTEMENT, IDUTILISATEUR, DATEDEBUTLOC FROM dbo.option_a_valider";
+	$result = odbc_exec($conn,$req);
+	echo '<table style="width:100%;"><TR><TD align="center"><b>Contrat</b></TD><TD align="center"><b>Appartement</b></TD><TD align="center"><b>Locataire Potentiel</b></TD><TD align="center"></TD></TR>';
+	while(odbc_fetch_row($result)){
+	  $contrat2 = odbc_result($result, 1);
+	  $appart2 = odbc_result($result, 2);
+	  $user2 = odbc_result($result, 3);
+
+	  echo '<tr><td align="center">'.$contrat2.'</td>
+	  		<td align="center">'.$appart2.'</td>
+			<td align="center">'.$user2.'</td>
+			<td align="center">
+				<form action="emp.php" method="post">
+					<input type="hidden" name="idValO" value="'.$contrat2.'">
+					<input type="image" src="img/valide.ico" height="15" width="15">
+				</form>
+			</td>
+			<td align="center">
+				<form action="emp.php" method="post">
+					<input type="hidden" name="idSupO" value="'.$contrat2.'">
 					<input type="image" src="img/delete.png" height="15" width="15">
 				</form>
 			</td>
